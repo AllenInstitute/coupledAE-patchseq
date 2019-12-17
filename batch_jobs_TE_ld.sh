531c0e8bd5bd776907106b13292bd43d6b32f2b7
Rohan <rhngla@gmail.com>
2019-12-17 00:21:11 -0800

    latent dimensionality tests script

---
+++ batch_jobs_TE_ld.sh
@@ -0,0 +1,31 @@
+for ld in 2 5
+do
+    for csTE in 1.0
+    do
+        for aE in 1.0
+        do
+            for cv in {0..8}
+            do
+                jobid="TE_v2_cv"$cv"_csTE_"$csTE"_aE_"$aE"_ld_"$ld
+                echo '#!/bin/bash'>subjob.bash
+                echo '#PBS -q celltypes'>>subjob.bash
+                echo '#PBS -N '${jobid//./-} >>subjob.bash
+                echo '#PBS -m a'>>subjob.bash
+                echo '#PBS -r n'>>subjob.bash
+                echo '#PBS -l ncpus=8'>>subjob.bash
+                echo '#PBS -l mem=16g,walltime=5:00:00'>>subjob.bash
+                echo '#PBS -o /allen/programs/celltypes/workgroups/mousecelltypes/Rohan/logs/'${jobid//./-}'.out'>>subjob.bash
+                echo '#PBS -j oe'>>subjob.bash
+                echo 'cd /allen/programs/celltypes/workgroups/mousecelltypes/Rohan/code/Patchseq-AE-Bioarxiv/'>>subjob.bash
+                echo 'source activate tf20-cpu'>>subjob.bash
+                echo 'python -m ae_model_train --batchsize 200 --cvfold '$cv' --alpha_T 1.0 --alpha_E '$aE' --alpha_M '$aE' --lambda_TE '$csTE' --latent_dim '$ld' --n_epochs 1500 --n_steps_per_epoch 500 --ckpt_save_freq 2000 --n_finetuning_steps 500 --run_iter 0 --model_id v2 --exp_name TE_Patchseq_Bioarxiv'>>subjob.bash
+                echo '...'
+                sleep 1
+                wait
+                qsub subjob.bash
+                echo 'Job: '${jobid//./-}' '
+            done
+        done
+    done
+done
+rm subjob.bash
